d9dfc1cb98080af12c038d51e5cef0082bcd5513
skarshankou <s.karshankou@skybuffer.com>
2025-01-31 08:14:53 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 function showPopover() {
     const login='stas';
     const password = '123456';
+    console.log(password);
     const popover = document.getElementById('popover');
     popover.style.display = 'block';
     setTimeout(() => {
